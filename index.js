3801ade99d40746a0595d7c489f6b500cd2876bc
Steven Lai <slioweng@gmail.com>
2017-09-15 10:20:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 const app = "I don't do much."
+
+var pbj = {
+  bread: "white",
+  ingredients: ["peanut butter", "jelly"],
+  cut: "triangles",
+  serve: function() {
+    console.log("here's your " + this.bread + ", enjoy!");
+  }
+}
